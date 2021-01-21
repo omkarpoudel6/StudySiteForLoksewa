be5fc35bdf6d9953f18cc64aedf9abb0b0cde9f6
omkarpoudel6 <omkar.poudel77@gmail.com>
2021-01-21 03:20:51 -0800

    modified learn page to hide the answer and show it when user clicks view answer button

--- Loksewa/admin.py
+++ Loksewa/admin.py
@@ -2,6 +2,25 @@ from django.contrib import admin
 from .models import Category,Questions,Answer
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Questions)
-admin.site.register(Answer)
+# admin.site.register(Category)
+# admin.site.register(Questions)
+# admin.site.register(Answer)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    readonly_fields = ['category',]
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    readonly_fields = ['question','answer1','answer2','answer3','answer4','correctAnswer',]
+
+class AnswerInline(admin.StackedInline):
+    model=Answer
+    # extra=1
+
+@admin.register(Questions)
+class QuestionsAdmin(admin.ModelAdmin):
+    inlines=[
+        AnswerInline,
+    ]
