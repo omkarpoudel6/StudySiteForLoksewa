fce7731bd9c57f32db423c0dadb912bdf2fa1c96
omkarpoudel6 <omkar.poudel77@gmail.com>
2021-01-27 06:54:55 -0800

    updating profile started

--- LoksewaPrepProject/settings.py
+++ LoksewaPrepProject/settings.py
@@ -41,8 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Loksewa',
-    'Users'
+    'Users',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK='bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- Users/forms.py
+++ Users/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+from .models import Profile
+
 
 class UserCreationForm(forms.ModelForm):
     """
@@ -31,15 +33,6 @@ class UserCreationForm(forms.ModelForm):
             )
         return password2
 
-    # def clean_email(self):
-    #     email=self.cleaned_data.get('email')
-    #     if User.objects.filter(email=email):
-    #         raise forms.ValidationError(
-    #             self.error_messages['email already used'],
-    #             code='email already used',
-    #         )
-    #     return email
-
     def save(self, commit=True):
         user = super(UserCreationForm, self).save(commit=False)
         user.set_password(self.cleaned_data["password1"])
@@ -47,19 +40,8 @@ class UserCreationForm(forms.ModelForm):
             user.save()
         return user
 
-# class RegistrationForm(UserCreationForm):
-#     email=forms.EmailField(required=True)
-#
-#     class Meta:
-#         model=User
-#         fields=('username','email','password1','password2')
-#
-#         def clean_email(self):
-#             email=self.cleaned_data['email']
-#             try:
-#                 User.objects.get(email=email)
-#                 raise forms.ValidationError("Email already exists")
-#             except User.MultipleObjectsReturned:
-#                 raise forms.ValidationError("Email already exists")
-#             except User.DoesNotExist:
-#                 pass
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model=Profile
+        exclude=('user',)
