063df3c0a12a8b278aa2076e94e8c39e498b7b72
omkarpoudel6 <omkar.poudel77@gmail.com>
2021-01-28 06:54:28 -0800

    sending verification mail started

--- LoksewaPrepProject/urls.py
+++ LoksewaPrepProject/urls.py
@@ -1,22 +1,11 @@
-"""LoksewaPrepProject URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 # from Loksewa.views import test,questions,category
+from . import settings
+
 from Loksewa import views
+from django.contrib.staticfiles.urls import static
 from Users.views import Login,Signup,Logout,Home
 
 urlpatterns = [
@@ -31,4 +20,4 @@ urlpatterns = [
     path('test/',views.test,name='test'),
     path('questions/',views.questions,name='questions'),
     path('category/<int:id>/',views.category,name='category')
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
