150b636ad492a39591662886a63ff8097b8ebee6
omkarpoudel6 <omkar.poudel77@gmail.com>
2021-01-28 08:02:17 -0800

    sending email verification done activating user remaining

--- LoksewaPrepProject/settings.py
+++ LoksewaPrepProject/settings.py
@@ -107,6 +107,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER='mailsend321@gmail.com'
+EMAIL_HOST_PASSWORD='nuppofftrbjtwsfb'
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_USE_SSL=False
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
